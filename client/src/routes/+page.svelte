be29823eb9ffa9c6989ebab03062f8e9036d333b
Robert Babaev <github@robertbabaev.tech>
2024-10-19 16:01:07 -0400

    scaffold tailwind + vitest testing

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -1,2 +1,16 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import { Alert } from 'flowbite-svelte';
+</script>
+
+<h1 class="text-3xl font-bold underline">Hello world!</h1>
+
+<Alert>
+	<span class="font-medium">Info alert!</span>
+	This is an alert.
+</Alert>
+
+<style lang="postcss">
+	:global(html) {
+		background-color: theme(colors.gray.100);
+	}
+</style>
